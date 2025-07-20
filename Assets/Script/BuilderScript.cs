c11fdc5cc5fa7a6630bedbdc9eba679afbedc478
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2025-07-20 17:30:02 +0300

    р

---
+++ Assets/Script/BuilderScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BuilderScript : MonoBehaviour
+{
+    [SerializeField] private GameObject BPLAModel;
+
+    //private GameObject[] motor; //nu ty ponyal
+    private GameObject[] propeller; //propeller
+    private GameObject[] krepezh; //krepezh
+    private GameObject[] poletstack; //holder
+    private GameObject[] core; //core
+    private GameObject[] furniture; //base
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //BPLAModel.GetComponentInChildren<GameObject>().CompareTag("Krepezh")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+
+}
