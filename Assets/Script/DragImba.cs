975a8e42b2e25d3a35173e0d63b7121d08cd858b
Temidze <94217293+Temidze@users.noreply.github.com>
2025-07-22 20:35:15 +0300

    Spizdily Olega

--- Assets/Script/DragImba.cs
+++ Assets/Script/DragImba.cs
@@ -3,26 +3,13 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 
-public class Example : MonoBehaviour, IBeginDragHandler , IDragHandler, IEndDragHandler
+public class Example : MonoBehaviour , IDragHandler
 {
-    private Vector3 position;
-    private float timeCount = 0.0f;
     public Transform mainObject;
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        position = transform.position;
-        Debug.Log("OnBeginDrag: " + position);
-    }
 
     // Drag the selected item.
     public void OnDrag(PointerEventData eventData)
     {
-        mainObject.eulerAngles += new Vector3(eventData.delta.y, eventData.delta.x);
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        transform.position = position;
-        Debug.Log("OnEndDrag: " + position);
+        mainObject.eulerAngles += new Vector3(eventData.delta.y, -eventData.delta.x);
     }
 }
