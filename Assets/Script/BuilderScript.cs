f26e86d4cf2bf70a0830b93bc17e795d533cb5b6
OlegWellDone <63550097+OlegWellDone@users.noreply.github.com>
2025-07-28 21:37:14 +0300

    ОбноваСкриптаБилдера, создание переходов для демки

--- Assets/Script/BuilderScript.cs
+++ Assets/Script/BuilderScript.cs
@@ -1,159 +1,109 @@
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BuilderScript : MonoBehaviour
 {
+
+    private Dictionary<int, List<GameObject>> stagesOfBuild = new Dictionary<int, List<GameObject>>();
     [SerializeField] private GameObject meObject;
     [SerializeField] private GameObject BPLAModel;
-
     [SerializeField] private Material NewMaterial;
-
     [SerializeField] private Material OldMaterial;
+
     //[SerializeField] string[] TagsInModel;//можем так задавать используемые тэги
-    //private GameObject[] motor; //nu ty ponyal
     private Infoobject InfoScript;
-    private List<GameObject> propeller = new List<GameObject>(); //propeller
-
-    private List<GameObject> krepezh = new List<GameObject>(); //krepezh
-
-    private List<GameObject> poletstack = new List<GameObject>(); //holder
-
-    private List<GameObject> core = new List<GameObject>(); //core
-
-    private List<GameObject> furniture = new List<GameObject>(); //base
-
 
     private int stage = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        List<string> AllTags = new List<string>();
         foreach (Transform child in BPLAModel.GetComponentsInChildren<Transform>(true))
         {
-            switch (child.gameObject.tag)
+            if (child.gameObject.tag == BPLAModel.tag)
             {
-                case "Krepezh":
-                    {
-                        krepezh.Add(child.gameObject);
-
-
-                    }
-                    break;
-                case "Propeller":
-                    {
-                        propeller.Add(child.gameObject);
-                    }
-                    break;
-                case "Holder":
-                    {
-                        poletstack.Add(child.gameObject);
-                    }
-                    break;
-                case "Core":
-                    {
-                        core.Add(child.gameObject);
-                    }
-                    break;
-                case "Base":
-                    {
-                        furniture.Add(child.gameObject);
-                    }
-                    break;
-                default:
-                    {
-
-                    }
-                    break;
+                continue;
             }
-            child.gameObject.SetActive(false);
+            AllTags.Add(child.gameObject.tag);
+        }
+        //просто вычитываем уникальные тэги
+        HashSet<string> uniqueTags = new HashSet<string>(AllTags);
+        AllTags.Clear();
+        AllTags = uniqueTags.ToList();
+
+        //заполняем тэги в мапу
+        for (int i = 0; i < AllTags.Count; i++)
+        {
+            List<GameObject> Temp = new List<GameObject>();
+            foreach (Transform child in BPLAModel.GetComponentsInChildren<Transform>(true))
+            {
+                if (child.gameObject.tag == AllTags[i])
+                {
+                    Temp.Add(child.gameObject);
+                    child.gameObject.SetActive(false);
+                }
+            }
+            stagesOfBuild.Add(i + 1, new List<GameObject>(Temp));
+            Temp.Clear();
         }
         BPLAModel.SetActive(true);
-        //BPLAModel.GetComponentInChildren<GameObject>().CompareTag("Krepezh")
+        //Debug.Log(stagesOfBuild.Count);
     }
 
     // Update is called once per frame
     void Update()
     {
-    }
-
-    private void SetOldModel(string previousTag)
-    {
-        foreach (Transform child in BPLAModel.GetComponentsInChildren<Transform>(true))
-        {
-            if (child.gameObject.tag == previousTag)
-            {
-            child.GetComponent<MeshRenderer>().material = OldMaterial;    
-            }
-        }
-    }
-
-    public void BuildBPLA()
-    {
-        switch (stage)
-        {
-            case 1:
-                {
-
-                    for (int i = 0; i < furniture.Count; i++)
-                    {
-                        furniture[i].SetActive(true);
-                        furniture[i].GetComponent<MeshRenderer>().material = NewMaterial;
-                        //Артём нахуевертил тута
-                        meObject.GetComponent<Infoobject>().setInfoObject(furniture[i]);
-                        //InfoScript.setInfoObject(furniture[i]);
-                        //Instantiate(furniture[i], new Vector3(1000, 1000, 1000), Quaternion.identity);
-                    }
-                    
-                }
-                break;
-            case 2:
-                {
-                    SetOldModel(furniture[0].tag);
-                    for (int i = 0; i < core.Count; i++)
-                    {
-                        core[i].SetActive(true);
-                        core[i].GetComponent<MeshRenderer>().material = NewMaterial;
-                        //Артём нахуевертил тута
-                        meObject.GetComponent<Infoobject>().setInfoObject(core[i]);
-                    }
-                }
-                break;
-            case 3:
-                {
-                    SetOldModel(core[0].tag);
-                    for (int i = 0; i < poletstack.Count; i++)
-                    {
-                        poletstack[i].SetActive(true);
-                        poletstack[i].GetComponent<MeshRenderer>().material = NewMaterial;
-                    }
-                }
-                break;
-            case 4:
-                {
-                    SetOldModel(poletstack[0].tag);
-                    for (int i = 0; i < krepezh.Count; i++)
-                    {
-                        krepezh[i].SetActive(true);
-                        krepezh[i].GetComponent<MeshRenderer>().material = NewMaterial;
-                    }
-                }
-                break;
-            case 5:
-                {
-                    SetOldModel(krepezh[0].tag);
-                    for (int i = 0; i < propeller.Count; i++)
-                    {
-                        propeller[i].SetActive(true);
-                        propeller[i].GetComponent<MeshRenderer>().material = NewMaterial;
-                    }
-                }
-                break;
-            case 6:
-                {
-                    SetOldModel(propeller[0].tag);
-                }
-                break;
-        }
         
-        stage++;
+    }
+
+    private void SetOldModel(int oldStage)
+    {
+        //Debug.Log("А мы ставим материал для прошлой детали");
+        for (int i = 0; i < stagesOfBuild[oldStage].Count; i++)
+        {
+            stagesOfBuild[oldStage][i].GetComponent<MeshRenderer>().material = OldMaterial;
+        }
+    }
+
+
+    private void DeactivateButton()
+    {
+        gameObject.SetActive(false);
+    }
+    public void BuildBPLA(bool next)
+    {
+        if (next && stage <= stagesOfBuild.Count)
+        {
+            //типо делаем следующий шаг
+            for (int i = 0; i < stagesOfBuild[stage].Count; i++)
+            {
+                stagesOfBuild[stage][i].SetActive(true);
+                stagesOfBuild[stage][i].GetComponent<MeshRenderer>().material = NewMaterial;
+
+                //Артём нахуевертил тута
+                //meObject.GetComponent<Infoobject>().setInfoObject(stagesOfBuild[stage]);
+            }
+            if (stage > 1)
+            {
+                SetOldModel(stage - 1);
+            }
+            stage++;
+        }
+        else if ((next == false) && (stage > 2))
+        {
+            //очевидно делаем предыдущий шаг
+            for (int i = 0; i < stagesOfBuild[stage - 1].Count; i++)
+            {
+                stagesOfBuild[stage - 1][i].SetActive(false);
+            }
+            for (int i = 0; i < stagesOfBuild[stage - 2].Count; i++)
+            {
+                stagesOfBuild[stage - 2][i].GetComponent<MeshRenderer>().material = NewMaterial;
+            }
+            ;
+            stage--;
+        }
     }
 }
--- Assets/Script/StartScanMenu.cs
+++ Assets/Script/StartScanMenu.cs
@@ -3,9 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class StartScanMenu : MonoBehaviour
 {
+    [Tooltip("На какую сцену будем переключаться: 0 - main menu, 1 - main scene, 2 - QRReader, 3 - QRGenerator")]
+    [SerializeField] int nextScene;
     public void ScanQR()
     {
         Debug.Log("ScanQR Code");
-       SceneManager.LoadScene("Assets/Scenes/MainScene.unity");
+        SceneManager.LoadScene("Assets/Scenes/MainScene.unity");
+    }
+
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene(nextScene);
     }
 }
