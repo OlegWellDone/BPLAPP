d0fc7079c559563e2c82b564063fdbe9fb3c2b1a
Temidze <94217293+Temidze@users.noreply.github.com>
2025-08-14 22:06:38 +0300

    Еще чутка

--- Assets/Script/DragImba.cs
+++ Assets/Script/DragImba.cs
@@ -12,7 +12,7 @@ public class Example : MonoBehaviour , IDragHandler
     {
         mainObject.transform.rotation *=  Quaternion.Euler(-eventData.delta.y, eventData.delta.x, 0);
         //mainObject.transform.rotation = new  Vector3(mainObject.eulerAngles.x,-eventData.delta.y, 0);
-        //mainObject.eulerAngles += new Vector3(1, 1);
-        Debug.Log(mainObject.eulerAngles );
+        //mainObject.eulerAngles += new Vector3(-eventData.delta.y, eventData.delta.x);
+        //Debug.Log(mainObject.eulerAngles );
     }
 }
