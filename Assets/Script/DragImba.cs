79bbd21f315eff1b1a11cbe7bf30807f9a2e8285
Temidze <94217293+Temidze@users.noreply.github.com>
2025-08-14 23:14:11 +0300

    Update DragImba.cs

--- Assets/Script/DragImba.cs
+++ Assets/Script/DragImba.cs
@@ -6,13 +6,12 @@ using UnityEngine.UI;
 public class Example : MonoBehaviour , IDragHandler
 {
     public Transform mainObject;
-
     // Drag the selected item.
     public void OnDrag(PointerEventData eventData)
     {
-        mainObject.transform.rotation *=  Quaternion.Euler(-eventData.delta.y, eventData.delta.x, 0);
+        //mainObject.transform.rotation *=  Quaternion.Euler(-eventData.delta.y, eventData.delta.x, 0);
         //mainObject.transform.rotation = new  Vector3(mainObject.eulerAngles.x,-eventData.delta.y, 0);
-        //mainObject.eulerAngles += new Vector3(-eventData.delta.y, eventData.delta.x);
+        mainObject.eulerAngles += new Vector3(-eventData.delta.y, eventData.delta.x);
         //Debug.Log(mainObject.eulerAngles );
     }
 }
