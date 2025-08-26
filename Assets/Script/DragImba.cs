6591217124b25e005fddbca590c8e73445605d6b
Temidze <94217293+Temidze@users.noreply.github.com>
2025-08-26 19:45:49 +0300

    Ограничители

--- Assets/Script/DragImba.cs
+++ Assets/Script/DragImba.cs
@@ -5,24 +5,31 @@ using UnityEngine.UI;
 using TMPro;
 
 
-public class DragImba : MonoBehaviour , IDragHandler
+public class DragImba : MonoBehaviour , IDragHandler, IEndDragHandler 
 {
     public Transform mainObject;
     public TextMeshProUGUI testText;
     private float touchDistance = 0;
     private bool kostil = false;
+    private float modScale = 0.2F;
+    private Vector2 swipeDelta;
+
     // Drag the selected item.
     public void OnDrag(PointerEventData eventData)
     {
+        //по факту не очень нужен именно свитчь но похрен
         switch (Input.touchCount)
         {
             case 2:
                 float delthaTouchDistance = touchDistance - Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
-                //testText.text = Input.GetTouch(0).position.ToString() + " " + Input.GetTouch(1).position.ToString();
+                testText.text = Input.GetTouch(0).position.ToString() + " " + Input.GetTouch(1).position.ToString();
                 touchDistance = Vector2.Distance(Input.GetTouch(0).position, Input.GetTouch(1).position);
                 if (kostil)
                 {
-                    mainObject.localScale += new Vector3(delthaTouchDistance, delthaTouchDistance, delthaTouchDistance);
+                    //ограничения
+                    if (mainObject.localScale.x+(delthaTouchDistance * modScale) >= 10F && mainObject.localScale.x+(delthaTouchDistance * modScale) <= 200F){
+                        mainObject.localScale += new Vector3(delthaTouchDistance * modScale, delthaTouchDistance * modScale, delthaTouchDistance * modScale);
+                    }
                 }
                 else
                 {
@@ -46,5 +53,10 @@ public class DragImba : MonoBehaviour , IDragHandler
         //mainObject.transform.rotation = new  Vector3(mainObject.eulerAngles.x,-eventData.delta.y, 0);
         //mainObject.eulerAngles += new Vector3(eventData.delta.y / 2, -eventData.delta.x / 2);
         //Debug.Log(mainObject.eulerAngles );
+    }
+    public void OnEndDrag(PointerEventData eventData) 
+    {
+        kostil = false;
+        //Vector2 swipeDelta = endPosition - startPosition;
     }
 }
