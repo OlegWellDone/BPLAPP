b8997cd8623d15774fb31948a35c906947950573
Temidze <94217293+Temidze@users.noreply.github.com>
2025-08-26 12:41:26 +0300

    Подготовка к скейлу пока работает пусть булет

--- Assets/Script/DragImba.cs
+++ Assets/Script/DragImba.cs
@@ -1,17 +1,37 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
 
 
-public class Example : MonoBehaviour , IDragHandler
+public class DragImba : MonoBehaviour , IDragHandler
 {
     public Transform mainObject;
+    public TextMeshProUGUI testText;
     // Drag the selected item.
     public void OnDrag(PointerEventData eventData)
     {
-        //mainObject.transform.rotation *=  Quaternion.Euler(-eventData.delta.y, eventData.delta.x, 0);
-        //mainObject.transform.rotation = new  Vector3(mainObject.eulerAngles.x,-eventData.delta.y, 0);
-        mainObject.eulerAngles += new Vector3(eventData.delta.y/2, -eventData.delta.x/2);
-        //Debug.Log(mainObject.eulerAngles );
-    }
+        switch (Input.touchCount)
+        {
+            case 2:
+                testText.text = Input.GetTouch(0).position.ToString() + " " + Input.GetTouch(1).position.ToString();
+                break;
+            default:
+                mainObject.eulerAngles += new Vector3(eventData.delta.y / 2, -eventData.delta.x / 2);
+                break;
+        }
+            
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                Debug.Log(touch.position.ToString());
+            }
+
+            //testText.text = SystemInfo.deviceType.ToString();
+            //mainObject.transform.rotation *=  Quaternion.Euler(-eventData.delta.y, eventData.delta.x, 0);
+            //mainObject.transform.rotation = new  Vector3(mainObject.eulerAngles.x,-eventData.delta.y, 0);
+            //mainObject.eulerAngles += new Vector3(eventData.delta.y / 2, -eventData.delta.x / 2);
+            //Debug.Log(mainObject.eulerAngles );
+        }
 }
